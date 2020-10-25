129cf0a454b49c6b2cd1c5a4a62d4b602f678ff0
Andrey <andreyshamray86@gmail.com>
2020-10-25 15:33:45 +0300

    finishing app

--- script.js
+++ script.js
@@ -6,29 +6,12 @@ const balance = document.getElementById('balance'),
     text = document.getElementById('text'),
     amount = document.getElementById('amount');
 
-const dummyTransactions = [{
-        id: 1,
-        text: 'Flower',
-        amount: -20
-    },
-    {
-        id: 2,
-        text: 'Salary',
-        amount: 300
-    },
-    {
-        id: 3,
-        text: 'Book',
-        amount: -10
-    },
-    {
-        id: 4,
-        text: 'Camera',
-        amount: 150
-    }
-];
+const localStorageTransactions = JSON.parse(
+    localStorage.getItem('transactions')
+);
 
-let transactions = dummyTransactions;
+let transactions =
+    localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
 function addTransaction(e) {
     e.preventDefault();
@@ -47,6 +30,8 @@ function addTransaction(e) {
         addTransactionDOM(transaction);
 
         updateValues();
+
+        updateLocalStorage();
 
         text.value = '';
         amount.value = '';
@@ -98,8 +83,12 @@ function updateValues() {
 
 function removeTransaction(id) {
     transactions = transactions.filter(transaction => transaction.id !== id);
-
+    updateLocalStorage();
     init();
+}
+
+function updateLocalStorage() {
+    localStorage.setItem('transactions', JSON.stringify(transactions));
 }
 
 function init() {
